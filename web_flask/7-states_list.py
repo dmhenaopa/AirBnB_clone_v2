75867370daab2f0336614cee870a540f3c28836b
Diana Henao <dmhenaopa@gmail.com>
2022-02-03 02:26:57 +0000

    :hammer: Add development script 7-states_list.py and html file templates/7-states_list.html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 """ Module that starts a Flask web application """
 from flask import Flask, escape, render_template
-
+from models import storage
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def display_greeting():
-    """Function that returns a greeting"""
-    return 'Hello HBNB!'
+@app.teardown_appcontext
+def remove_session(self):
+    """Function that remove the current SQLALchemy
+       Session
+    """
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def display_():
+    """Function that display a html page"""
+    states = storage.all('State').values()
+    return render_template('7-states_list.html',
+                           states=states)
 
 
 if __name__ == '__main__':
